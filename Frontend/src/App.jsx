465f9c5970ca1261ede2a9538f6e82578e75161f
PrithwishDas101 <prithwish6636@gmail.com>
2026-05-01 15:39:31 +0530

    implemented favourites page functionality

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,13 +2,14 @@ import './css/App.css'
 import Favourites from './pages/Favourites.jsx'
 import Home from './pages/Home.jsx'
 import { Routes, Route } from 'react-router-dom'
+import { MovieProvider } from './contexts/MovieContext.jsx'
 import NavBar from './components/NavBar.jsx'
 
 function App() {
 
   return (
     <>
-      <div>
+      <MovieProvider>
         <NavBar />
         <main className='main-content'>
           <Routes>
@@ -16,7 +17,8 @@ function App() {
             <Route path="/favourites" element={<Favourites />} />
           </Routes>
         </main>
-      </div>
+      </MovieProvider>
+
     </>
   )
 }
--- Frontend/src/pages/Favourites.jsx
+++ Frontend/src/pages/Favourites.jsx
@@ -1,14 +1,29 @@
-import "../css/Favourites.css"
+import "../css/Favourites.css";
+import { useMovieContext } from "../contexts/MovieContext.jsx";
+import MovieCard from "../components/MovieCard.jsx";
 
-function Favourites () {
+function Favourites() {
+    const { favourites } = useMovieContext();
+
     return (
-        <>
-        <div className="favourites-empty">
-            <h2><b>No favourite movies yet</b></h2>
-            <p>Add movies to your favourites to see them here.</p>
+        <div className="favourites">
+            {favourites.length === 0 ? (
+                <div className="favourites-empty">
+                    <h2><b>No favourite movies yet</b></h2>
+                    <p>Add movies to your favourites to see them here.</p>
+                </div>
+            ) : (
+                <>
+                    <h2>Your Favourites</h2>
+                    <div className="movies-grid">
+                        {favourites.map((movie) => (
+                            <MovieCard movie={movie} key={movie.id} />
+                        ))}
+                    </div>
+                </>
+            )}
         </div>
-        </>
-    )
+    );
 }
 
-export default Favourites
+export default Favourites;
